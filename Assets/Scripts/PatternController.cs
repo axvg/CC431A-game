a26df2aa9c7e07114ba3df30da75b780ca103654
Alex Vega <axvg@users.noreply.github.com>
2025-11-26 21:52:56 -0500

    levels

--- Assets/Scripts/PatternController.cs
+++ Assets/Scripts/PatternController.cs
@@ -1,22 +1,81 @@
 using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable] 
+public class Wave
+{
+    public string name;
+    public BasePattern pattern;
+    public float duration;
+    public float fireRate;
+}
 
 public class PatternController : MonoBehaviour
 {
+    [Header("level settings")]
+    public List<Wave> levelWaves;
+    public bool loopLevel = true;
+
+    private int currentWaveIndex = 0;
+    private float waveTimer = 0f;
+    private float fireTimer = 0f;
+
     public float fireRate = .5f;
     private float timer;
 
-    public BasePattern pattern;
+    // public BasePattern pattern;
+
+    void Start()
+    {
+        currentWaveIndex = 0;
+        waveTimer = 0f;
+        fireTimer = 0f;
+    }
 
     void Update()
     {
-        timer += Time.deltaTime;
-        if (timer >= fireRate)
+        if (levelWaves.Count == 0)
+            return;
+
+        Wave currentWave = levelWaves[currentWaveIndex];
+        fireTimer += Time.deltaTime;
+
+        if (fireRate > 0f && fireTimer >= currentWave.fireRate)
         {
-            if (pattern != null)
+            if (currentWave.pattern != null)
             {
-                pattern.Trigger(transform);
+                currentWave.pattern.Trigger(transform);
             }
-            timer = 0f;
+            fireTimer = 0f;
+        }
+
+        waveTimer += Time.deltaTime;
+        if (waveTimer >= currentWave.duration)
+        {
+            NextWave();
+        }
+    }
+
+    void NextWave()
+    {
+        waveTimer = 0f;
+        currentWaveIndex++;
+
+        if (currentWaveIndex >= levelWaves.Count)
+        {
+            if (loopLevel)
+            {
+                currentWaveIndex = 0;
+                Debug.Log("-> restarted level");
+            }
+            else
+            {
+                currentWaveIndex = levelWaves.Count - 1;
+                Debug.Log("-> end of level");
+            }
+        } else
+        {
+            Debug.Log("-> start wave: " + levelWaves[currentWaveIndex].name);
         }
     }
 }
