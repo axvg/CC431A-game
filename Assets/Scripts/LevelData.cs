990f6e5c599a4acecda34528c669856dbe7af6a6
Alex Vega <axvg@users.noreply.github.com>
2025-11-27 22:03:36 -0500

    add level data

--- Assets/Scripts/LevelData.cs
+++ Assets/Scripts/LevelData.cs
@@ -15,4 +15,30 @@ public class LevelData : ScriptableObject
 {
     public string levelName = "Nivel 1";
     public List<Wave> waves;
+
+    [Header("Level info")]
+    [TextArea]
+    public string levelInfo;
+
+    void OnValidate()
+    {
+        float totalSeconds = 0f;
+        int totalEnemiesMin = 0;
+        foreach (var wave in waves)
+        {
+            totalSeconds += wave.duration;
+
+            if (wave.spawnRate > 0)
+            {
+                totalEnemiesMin += Mathf.CeilToInt(wave.duration / wave.spawnRate);
+            }
+        }
+
+        // format
+        int minutes = Mathf.FloorToInt(totalSeconds / 60);
+        int seconds = Mathf.FloorToInt(totalSeconds % 60);
+
+        levelInfo = $"duracion total: {totalSeconds} seg ({minutes}:{seconds:00})\n" +
+                    $"enemies aprox: {totalEnemiesMin}";
+    }
 }
