f5f899902f284ffe921ee11b32b3bd9fe02172d7
Alex Vega <axvg@users.noreply.github.com>
2025-11-28 13:06:49 -0500

    alpha

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,9 @@ public class PlayerController : MonoBehaviour
     public BulletColor playerColor;
     private SpriteRenderer sr;
 
+    [Header("hitbox")]
+    public GameObject hitboxVisual;
+    private SpriteRenderer hitboxSr;
 
     private List<BulletColor> availableColors = new List<BulletColor>();
     private int colorIndex = 0;
@@ -23,6 +26,11 @@ public class PlayerController : MonoBehaviour
     {
         sr = GetComponent<SpriteRenderer>();
         // UpdateColorVisuals();
+        if (hitboxVisual != null)
+        {
+            hitboxSr = hitboxVisual.GetComponent<SpriteRenderer>();
+            hitboxVisual.SetActive(false);
+        }
     }
 
     public void ConfigureAbilities(List<BulletColor> newColors, bool enableFocus) 
@@ -85,11 +93,21 @@ public class PlayerController : MonoBehaviour
             // currentSpeed = focusSpeed;
             Time.timeScale = slowMotionFactor;
             Time.fixedDeltaTime = 0.02f * Time.timeScale;
+            if(hitboxSr != null) hitboxSr.color = Color.white;
+
+            Color transparentColor = sr.color;
+            transparentColor.a = 0.1f;
+            sr.color = transparentColor;
         }
         else
         {
             Time.timeScale = 1f;
             Time.fixedDeltaTime = 0.02f;
+            if (hitboxVisual != null) hitboxVisual.SetActive(false);
+
+            Color opaqueColor = sr.color;
+            opaqueColor.a = 1f; 
+            sr.color = opaqueColor;
         }
 
         float x = Input.GetAxis("Horizontal");
@@ -155,6 +173,18 @@ public class PlayerController : MonoBehaviour
                 }
                 bullet.ReturnToPool();
             }
+        }
+    }
+
+    // helper
+    Color GetColorFromEnum(BulletColor c)
+    {
+        switch (c)
+        {
+            case BulletColor.Red: return Color.red;
+            case BulletColor.Blue: return Color.blue;
+            case BulletColor.Green: return Color.green;
+            default: return Color.white;
         }
     }
 }
